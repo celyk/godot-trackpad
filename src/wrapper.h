c83ea79991258e3480885c099ab0b268b1c237dc
celyk <humanimations10@gmail.com>
2025-12-29 20:11:44 +1100

    Add OMGTouchData

--- src/wrapper.h
+++ src/wrapper.h
@@ -3,8 +3,68 @@
 
 //#include <godot_cpp/core/version.hpp>
 #include <godot_cpp/core/class_db.hpp>
+#include <godot_cpp/classes/ref.hpp>
 
 namespace godot{
+
+/*
+enum OMSState: String, Sendable {
+    case notTouching
+    case starting
+    case hovering
+    case making
+    case touching
+    case breaking
+    case lingering
+    case leaving
+}
+*/
+
+class OMSTouchData : public RefCounted {
+	GDCLASS(OMSTouchData, RefCounted);
+
+    int id; //: Int32
+    Vector2 position;
+    float total; // total value of capacitance
+    float pressure;
+    Vector2 axis;
+    float angle; // finger angle
+    float density; // area density of capacitance
+    int state; //: OMSState
+    String timestamp;
+
+protected:
+	static void _bind_methods();
+
+public:
+    int get_id() const;
+    void set_id(int p_id);
+    
+    Vector2 get_position() const;
+    void set_position(int p_position);
+    
+    float get_total() const;
+    void set_total(float p_total);
+    
+    float get_pressure() const;
+    float set_pressure(float p_pressure);
+
+    Vector2 get_axis() const;
+    void set_axis(Vector2 p_axis);
+
+    float get_angle() const;
+    void set_angle(float p_angle);
+
+    float get_density() const;
+    void set_density(float p_density);
+
+    int get_state() const;
+    void set_state(int p_state);
+
+    String get_timestamp() const;
+    void set_timestamp(String p_timestamp);
+};
+
 
 class TrackpadServer : public Object {
 	GDCLASS(TrackpadServer, Object);
--- src/wrapper.mm
+++ src/wrapper.mm
@@ -10,3 +10,65 @@ void TrackpadServer::_bind_methods() {
 void TrackpadServer::registerInputCallback(Callable callback) {
 
 }
+
+void OMSTouchData::_bind_methods() {
+	ADD_PROPERTY(PropertyInfo(Variant::INT, "id"), "set_id", "get_id");
+	ADD_PROPERTY(PropertyInfo(Variant::VECTOR3, "position"), "set_id", "get_id");
+	ADD_PROPERTY(PropertyInfo(Variant::INT, "total"), "set_id", "get_id");
+	ADD_PROPERTY(PropertyInfo(Variant::INT, "pressure"), "set_id", "get_id");
+	ADD_PROPERTY(PropertyInfo(Variant::INT, "axis"), "set_id", "get_id");
+	ADD_PROPERTY(PropertyInfo(Variant::INT, "angle"), "set_id", "get_id");
+	ADD_PROPERTY(PropertyInfo(Variant::INT, "density"), "set_id", "get_id");
+	ADD_PROPERTY(PropertyInfo(Variant::INT, "state"), "set_id", "get_id");
+	ADD_PROPERTY(PropertyInfo(Variant::INT, "timestamp"), "set_id", "get_id");
+}
+
+
+int OMSTouchData::get_id() const
+{
+    return id;
+}
+void OMSTouchData::set_id(int p_id)
+{	
+	id = p_id;
+}
+
+Vector2 OMSTouchData::get_position() const
+{
+    return position;
+}
+
+float OMSTouchData::get_total() const
+{
+    return total;
+}
+
+float OMSTouchData::get_pressure() const
+{
+    return pressure;
+}
+
+Vector2 OMSTouchData::get_axis() const
+{
+    return axis;
+}
+
+float OMSTouchData::get_angle() const
+{
+    return angle;
+}
+
+float OMSTouchData::get_density() const
+{
+    return density;
+}
+
+int OMSTouchData::get_state() const
+{
+    return state;
+}
+
+String OMSTouchData::get_timestamp() const
+{
+    return timestamp;
+}

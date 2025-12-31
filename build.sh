debb4b98b4907c8a4b8d351c952c135ee6e2c808
celyk <humanimations10@gmail.com>
2025-12-31 21:58:41 +1100

    Fix build system

--- build.sh
+++ build.sh
@@ -5,11 +5,14 @@ set -e
 git submodule update --init --recursive
 
 # Build the framework.
-cd ./OpenMultitouchSupport
+cd ./dependencies/OpenMultitouchSupport
 ./build_framework.sh
 
 # Change back to the root directory.
 cd ..
+
+# Copy out the newly built framework to be linked to our GDExtension
+cp ./dependencies/OpenMultitouchSupport/Framework/build/Build/Products/Release ./dependencies
 
 # Build the GDExtension.
 scons

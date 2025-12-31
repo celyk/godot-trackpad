90b325e4997afc1f4269cadf1017880fe151e94c
celyk <humanimations10@gmail.com>
2025-12-31 20:49:02 +1100

    Update build script

--- build.sh
+++ build.sh
@@ -1,3 +1,15 @@
+#!/bin/bash
+set -e
+
+# Initialize our repo just in case
+git submodule update --init --recursive
+
+# Build the framework
 cd ./OpenMultitouchSupport
 ./build_framework.sh
 
+# Change back to the root directory
+cd ..
+
+# Build the GDExtension
+scons

084e65e5ada7ae2ebeb4bf6edebacaa72061733b
celyk <humanimations10@gmail.com>
2026-02-07 12:36:38 +1100

    press escape to uncapture

--- addons/godot_trackpad/autoload.gd
+++ addons/godot_trackpad/autoload.gd
@@ -22,6 +22,13 @@ signal trackpad_touch(touch:TrackpadTouch)
 const TouchscreenEmulation = preload("uid://bhwwj71jhevtg")
 const TouchscreenEmulationDebugDraw = preload("uid://b1jy66mvled8i")
 
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey and event.keycode == KEY_ESCAPE and event.is_pressed():
+			if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+				Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+			else:
+				Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
 func _ready() -> void:
 	var device := TrackpadServer.get_primary_device()
 	

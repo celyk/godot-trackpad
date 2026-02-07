ee003088646e659a5a82df52e02163dbbceb4480
celyk <humanimations10@gmail.com>
2026-02-07 13:50:39 +1100

    Update

--- addons/godot_trackpad/autoload.gd
+++ addons/godot_trackpad/autoload.gd
@@ -24,10 +24,10 @@ const TouchscreenEmulationDebugDraw = preload("uid://b1jy66mvled8i")
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey and event.keycode == KEY_ESCAPE and event.is_pressed():
-			if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
-				Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-			else:
-				Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		else:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _ready() -> void:
 	var device := TrackpadServer.get_primary_device()
